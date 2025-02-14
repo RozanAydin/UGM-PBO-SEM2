5bc876af09af0bd76d15514ccdab0dcd15d1bc95
chun <rozanaydinz@gmail.com>
2025-02-14 23:10:13 +0700

    Update lat1.py

--- Pertemuan1/lat1.py
+++ Pertemuan1/lat1.py
@@ -1,9 +1,4 @@
-while True:
-    try:
-        angka = int(input("Masukkan angka: "))
-        for i in range(angka + 1):
-            print(angka, end = " ")
-            angka = angka - 1
-        break
-    except:
-        print("masukkan dalam bentuk angka!")
+angka = int(input("Masukkan angka: "))
+for i in range(angka + 1):
+    print(angka, end = " ")
+    angka = angka - 1
